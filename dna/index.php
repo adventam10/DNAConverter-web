74ef5fe0d8455c24c7e6e5cfe929171108cdd1dd
am10 <adventam10@gmail.com>
2020-01-25 00:55:22 +0900

    Add app store links

--- dna/index.php
+++ dna/index.php
@@ -120,6 +120,11 @@ function getTwitterURL() {
       float: right;
     }
     .contents{
+      margin-top: 16px;
+      margin-bottom: 16px;
+    }
+    .store_badge_area{
+      vertical-align: middle;
       margin-bottom: 32px;
     }
     @media screen and (max-width:480px) {
@@ -152,6 +157,11 @@ function getTwitterURL() {
         <input class="button button-convert" type="submit" value="変換する">
       </form>
       <button class="button tweet" type=“button” onclick="location.href='<?php echo getTwitterURL();?>'"><img src="img/icon_twitter.png" alt="ツイッター" /></button>
+    </div>
+    <div class="store_badge_area">
+      <a href="https://apps.apple.com/jp/app/dna%E5%A4%89%E6%8F%9B/id1493994947?mt=8" style="display:inline-block;overflow:hidden;background:url(https://linkmaker.itunes.apple.com/ja-jp/badge-lrg.svg?releaseDate=2020-01-22&kind=iossoftware&bubble=ios_apps) no-repeat;width:135px;height:40px;margin:8px;background-size:contain;"></a>
+      <a href="https://apps.apple.com/jp/app/dna%E5%A4%89%E6%8F%9B/id1494127578?mt=12" style="display:inline-block;overflow:hidden;background:url(img/badge_mac_app_store.svg) no-repeat;width:135px;height:40px;margin:8px;background-size:contain;"></a>
+      <a href='https://play.google.com/store/apps/details?id=am10.dnaconverter&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Google Play で手に入れよう' width="144px" height="56px" src='https://play.google.com/intl/us-en/badges/static/images/badges/ja_badge_web_generic.png'/></a>
     </div>
     <footer>
       <hr class="header-footer">
