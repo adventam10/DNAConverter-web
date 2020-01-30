79cf697283b2bee461a0d1915499916326570644
am10 <adventam10@gmail.com>
2020-01-30 20:18:42 +0900

    Support dark mode

--- src/index.php
+++ src/index.php
@@ -8,8 +8,14 @@
 <div class="contents">
   <form class="form-convert" action="index.php" method="post">
     <div class="radio-group">
-      <input type="radio" id="radio1" name="mode" value="0" <?php if($mode==0) echo 'checked'?>> <label for="radio1">ことば</label>
-      <input type="radio" id="radio2" name="mode" value="1" <?php if($mode==1) echo 'checked'?>> <label for="radio2">ぬくれおちど</label>
+      <label class="label-radio">
+        <input type="radio" name="mode" value="0" <?php if($mode==0) echo 'checked'?>>
+        <span class="lever">ことば</span>
+      </label>
+      <label class="label-radio">
+        <input type="radio" name="mode" value="1" <?php if($mode==1) echo 'checked'?>>
+        <span class="lever">ぬくれおちど</span>
+      </label>
     </div>
     <div class="text-area">
       <textarea class="text-input" placeholder="入力してください" rows="10" name="value"><?php if(!empty($_POST['value'])) echo $_POST['value']?></textarea>
@@ -25,8 +31,8 @@
   <button class="button button-tweet" type=“button” onclick="location.href='<?php echo getTwitterURL();?>'"><img src="img/icon_twitter.png" alt="ツイッター" /></button>
 </div>
 <div class="store-badge-area">
-  <a href="https://apps.apple.com/jp/app/dna%E5%A4%89%E6%8F%9B/id1493994947?mt=8" style="display:inline-block;overflow:hidden;background:url(https://linkmaker.itunes.apple.com/ja-jp/badge-lrg.svg?releaseDate=2020-01-22&kind=iossoftware&bubble=ios_apps) no-repeat;width:135px;height:40px;margin:8px;background-size:contain;"></a>
-  <a href="https://apps.apple.com/jp/app/dna%E5%A4%89%E6%8F%9B/id1494127578?mt=12" style="display:inline-block;overflow:hidden;background:url(img/badge_mac_app_store.svg) no-repeat;width:135px;height:40px;margin:8px;background-size:contain;"></a>
+  <a href="https://apps.apple.com/jp/app/dna%E5%A4%89%E6%8F%9B/id1493994947?mt=8" class="apple-badge" style="background:url(https://linkmaker.itunes.apple.com/ja-jp/badge-lrg.svg?releaseDate=2020-01-22&kind=iossoftware&bubble=ios_apps) no-repeat;width:135px;"></a>
+  <a href="https://apps.apple.com/jp/app/dna%E5%A4%89%E6%8F%9B/id1494127578?mt=12" class="apple-badge" style="background:url(img/badge_mac_app_store.svg) no-repeat;width:140px;"></a>
   <a href='https://play.google.com/store/apps/details?id=am10.dnaconverter&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Google Play で手に入れよう' width="144px" height="56px" src='https://play.google.com/intl/us-en/badges/static/images/badges/ja_badge_web_generic.png'/></a>
 </div>
 <?php require('php/parts/footer.php');?>
