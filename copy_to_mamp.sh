e96597ade468b016995f6509684af41946c9faf9
am10 <adventam10@gmail.com>
2020-01-30 19:26:45 +0900

    Remove dna directory

--- copy_to_mamp.sh
+++ copy_to_mamp.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rm -r /Applications/MAMP/htdocs/dna
-cp -r dna/dist /Applications/MAMP/htdocs/
+cp -r dist /Applications/MAMP/htdocs/
 mv /Applications/MAMP/htdocs/dist /Applications/MAMP/htdocs/dna
